a3975bb825925f70ca27e178ccb2192757fba97a
mohammed <el.moutaouakkil@hotmail.com>
2024-04-07 22:11:16 +0000

    first commit: project setup

---
+++ src/app-info.tsx
@@ -0,0 +1,5 @@
+const appInfo = {
+    title: '.&#x2F;'
+};
+export default appInfo;
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export { default as HomePage } from "./home/home";
+export { default as ProfilePage } from "./flights/flights";
+export { default as TasksPage } from "./tasks/tasks";
