a9c04f28c0e3ac5dea7cd0f3ab0c5c8a5ffb4422
El-Moutaouakkil <el.moutaouakkil@hotmail.com>
2024-04-08 21:16:08 +0000

    chore: update dependencies in package.json and fix test in App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,10 @@
 import './matchMediaMock';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { act } from 'react-dom/test-utils';
 
 describe("App", () => {
   test('renders learn react link', async () => {
-    await act( async () => { render(<App/>) });
+    await render(<App/>);
     const linkElement = screen.getByText(/create react app/i);
     expect(linkElement).toBeInTheDocument();
   });
